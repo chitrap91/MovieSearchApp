fba6e78b15d446402cd0c90e8bab5ab68dd2d026
Chitadevi <chitrakrishnan1291@gmail.com>
2025-09-21 20:26:09 -0500

    cleaned up commented code

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,8 +5,6 @@ import { FaHome, FaHeart } from 'react-icons/fa'; // 🔹 Home & Favourite icons
 function Header() {
 
     return (
-
-        // <div className="max-w-5xl mx-auto flex justify-center items-center py-8 flex-col">
         <div className="max-w-5xl mx-auto flex justify-between items-center py-4 px-6">
             {/* <div className="flex justify-evenly"> */}
             <div className="flex items-center justify-between w-full">
@@ -16,7 +14,6 @@ function Header() {
                     </span>
                 </Link>
             </div>
-            {/* <h1 className="poppins-font text-5xl font-bold text-red-500 text-center w-3/4">Welcome to Movie Finder</h1> */}
             {/* Nav Icons */}
             <div className="flex gap-4">
                 {/* Home */}
@@ -35,16 +32,6 @@ function Header() {
                     <FaHeart className="text-red-500 hover:text-white" size={18} />
                 </Link>
             </div>
-            {/* <div className="w-8 h-8 rounded-full flex items-center justify-center  bg-white hover:bg-red-500 transition-colors">
-
-                    <Link
-                        to={`/favourite-movies`}
-                    >
-                        <button> <img src={favIcon} alt="favourite" className="w-4 h-4" /></button>
-                    </Link>
-                </div> */}
-            {/* </div> */}
-            {/* <p className=" mt-5 poppins-font text-sm font-bold text-red-500 text-center">Discover movies , series and episodes from the OMDB Database </p> */}
         </div>
     )
 }
